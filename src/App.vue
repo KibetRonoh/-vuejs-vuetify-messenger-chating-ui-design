9d931ea261abbe7dc58275871d3c58069cd873e8
kibet hillary <kibethillaire@gmail.com>
2022-02-26 00:13:53 +0300

    update home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<template>
-  <v-app>
-    <SideBar />
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-  </v-app>
-</template>
 
 <script>
 import SideBar from './components/SideBar';
